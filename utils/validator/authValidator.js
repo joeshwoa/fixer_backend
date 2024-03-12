445e0d9b7cc03d25b99ed201ce878d9778cec6a2
joeshwoa <96644682+joeshwoa@users.noreply.github.com>
2024-03-12 12:24:48 +0200

    Update authValidator.js

--- utils/validator/authValidator.js
+++ utils/validator/authValidator.js
@@ -1,6 +1,6 @@
 const slugify = require("slugify");
 const { check } = require("express-validator");
-const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const validatorMiddleware = require("../../middlewares/validatorMiddleWare");
 const User = require("../../models/userModel");
 
 exports.signupValidator = [
